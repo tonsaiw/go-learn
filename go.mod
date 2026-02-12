4f78cfa38b4a1aad9144e0335472bc5c99094dc4
tonsaiw <tonsai10538@gmail.com>
2026-02-13 04:43:07 +0700

    add server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/tonsaiw/go-learn
+
+go 1.25.0
--- main.go
+++ main.go
@@ -1,7 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
+
 
 func main() {
+	server := &http.Server{
+		Addr: ":3000",
+		Handler: http.HandlerFunc(basicHandler),
+	}
+
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+	}
 	fmt.Println("Hello, World!")
+}
+
+func basicHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello, World!"))
 }
